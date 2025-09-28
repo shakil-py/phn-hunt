70d3730df7f726196479c1c707099b1eb7022ef0
Shakil Haque <shakilhaquepy@gmail.com>
2025-09-28 20:53:53 +0600

    add search option

--- app/js/app.js
+++ app/js/app.js
@@ -1,40 +1,21 @@
 // console.log("hello world");
-const loadePhone = async () => {
-    const response = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async (searchText) => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await response.json();
     displayPhones(data.data);
-}
-loadePhone();
-// function displayPhones(phones) {
-//     const phoneContainer = document.getElementById('phone-container');
-//     for (const phone of phones) {
-//         console.log(phone);
-//         const phoneDiv = document.createElement('div');
-//         phoneDiv.classList.add('phone', 'rounded-lg', 'shadow-lg');
-//         phoneDiv.id = 'phone';
-//         phoneDiv.innerHTML = `
-//         <div class="flex flex-col bg-white justify-center items-center  p-5 items-center  mx-auto m-10 border-white">
-//         <img class="" src="${phone.image}" alt="">
-//     <h3 class=" text-center font-bold ">${phone.phone_name}</h3>
-//     <p>Brand: ${phone.brand}</p>
+};
 
-//     </div>
-//     `;
-//         phoneContainer.appendChild(phoneDiv);
-
-
-
-//     }
-function displayPhones(phones) {
+function displayPhones(searchText) {
     const phoneContainer = document.getElementById('phone-container');
-    for (const phone of phones) {
+    phoneContainer.textContent = '';
+    for (const phone of searchText) {
         console.log(phone);
         const phoneDiv = document.createElement('div');
         phoneDiv.classList.add('phone', 'rounded-lg', 'shadow-lg');
         phoneDiv.id = 'phone';
         phoneDiv.innerHTML = `
         <div class="card bg-white  shadow-sm mt-16">
-  <figure>
+    <figure>
     <img class="w-1/2 h-1/2 mt-5"
       src=${phone.image} />
   </figure>
@@ -50,14 +31,13 @@ function displayPhones(phones) {
     `;
         phoneContainer.appendChild(phoneDiv);
 
-
-
     }
+    
 
-}
-{/* <div class="flex flex-col bg-white justify-center items-center  p-5 items-center  mx-auto m-10 border-white">
-    <img class="" src="${phone.image}" alt="">
-        <h3 class=" text-center font-bold ">${phone.phone_name}</h3>
-        <p>Brand: ${phone.brand}</p>
-
-</div> */}
+};
+const searchPhone = async () => {
+    const searchField = document.getElementById('search-phn');
+    const searchText = searchField.value;
+    searchField.value = '';
+    loadPhone(searchText);
+};
