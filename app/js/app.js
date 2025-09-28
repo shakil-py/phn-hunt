7c008f889f1f4d8ef7b0b576b28d32f7deb111f7
Shakil Haque <shakilhaquepy@gmail.com>
2025-09-28 18:11:41 +0600

    add more style

--- app/js/app.js
+++ app/js/app.js
@@ -33,13 +33,13 @@ function displayPhones(phones) {
         phoneDiv.classList.add('phone', 'rounded-lg', 'shadow-lg');
         phoneDiv.id = 'phone';
         phoneDiv.innerHTML = `
-        <div class="card bg-base-100  shadow-sm mt-16">
+        <div class="card bg-white  shadow-sm mt-16">
   <figure>
-    <img
+    <img class="w-1/2 h-1/2 mt-5"
       src=${phone.image} />
   </figure>
   <div class="card-body">
-    <h2 class="flex justify-center m-10 card-title">${phone.phone_name}</h2>
+    <h2 class="flex justify-center m-10 card-title font-black text-black">${phone.phone_name}</h2>
     <div class=" card-actions justify-end">
       <button class="btn btn-primary w-full bg-amber-500 text-white 
         
