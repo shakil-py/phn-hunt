4669b65d0fe05ad265247c3371efc12eb566012e
Shakil Haque <shakilhaquepy@gmail.com>
2025-09-28 17:14:27 +0600

    using phn hunter show card and style also

--- app/js/app.js
+++ app/js/app.js
@@ -5,6 +5,26 @@ const loadePhone = async () => {
     displayPhones(data.data);
 }
 loadePhone();
+// function displayPhones(phones) {
+//     const phoneContainer = document.getElementById('phone-container');
+//     for (const phone of phones) {
+//         console.log(phone);
+//         const phoneDiv = document.createElement('div');
+//         phoneDiv.classList.add('phone', 'rounded-lg', 'shadow-lg');
+//         phoneDiv.id = 'phone';
+//         phoneDiv.innerHTML = `
+//         <div class="flex flex-col bg-white justify-center items-center  p-5 items-center  mx-auto m-10 border-white">
+//         <img class="" src="${phone.image}" alt="">
+//     <h3 class=" text-center font-bold ">${phone.phone_name}</h3>
+//     <p>Brand: ${phone.brand}</p>
+
+//     </div>
+//     `;
+//         phoneContainer.appendChild(phoneDiv);
+
+
+
+//     }
 function displayPhones(phones) {
     const phoneContainer = document.getElementById('phone-container');
     for (const phone of phones) {
@@ -13,12 +33,21 @@ function displayPhones(phones) {
         phoneDiv.classList.add('phone', 'rounded-lg', 'shadow-lg');
         phoneDiv.id = 'phone';
         phoneDiv.innerHTML = `
-        <div class="flex flex-col  p-5 items-center w-96 mx-auto m-10 border-white">
-        <img class="" src="${phone.image}" alt="">
-    <h3 class=" text-center font-bold ">${phone.phone_name}</h3>
-    <p>Brand: ${phone.brand}</p>
-    
+        <div class="card bg-base-100  shadow-sm">
+  <figure>
+    <img
+      src=${phone.image} />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title items-center">${phone.phone_name}</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary w-full bg-amber-500 text-white 
+        
+      }">Buy Now</button>
     </div>
+  </div>
+</div>
     `;
         phoneContainer.appendChild(phoneDiv);
 
@@ -27,3 +56,9 @@ function displayPhones(phones) {
     }
 
 }
+{/* <div class="flex flex-col bg-white justify-center items-center  p-5 items-center  mx-auto m-10 border-white">
+    <img class="" src="${phone.image}" alt="">
+        <h3 class=" text-center font-bold ">${phone.phone_name}</h3>
+        <p>Brand: ${phone.brand}</p>
+
+</div> */}
