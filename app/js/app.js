bfba5434c1d4d95bdb6ea2d2521325eff924de9a
Shakil Haque <shakilhaquepy@gmail.com>
2025-09-29 12:23:18 +0600

    add spiner

--- app/js/app.js
+++ app/js/app.js
@@ -1,19 +1,19 @@
 // console.log("hello world");
 const loadPhone = async (searchText) => {
-    const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
-    const data = await response.json();
-    displayPhones(data.data);
+  const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
+  const data = await response.json();
+  displayPhones(data.data);
 };
 
 function displayPhones(searchText) {
-    const phoneContainer = document.getElementById('phone-container');
-    phoneContainer.textContent = '';
-    for (const phone of searchText) {
-        console.log(phone);
-        const phoneDiv = document.createElement('div');
-        phoneDiv.classList.add('phone', 'rounded-lg', 'shadow-lg');
-        phoneDiv.id = 'phone';
-        phoneDiv.innerHTML = `
+  const phoneContainer = document.getElementById('phone-container');
+  phoneContainer.textContent = '';
+  for (const phone of searchText) {
+    console.log(phone);
+    const phoneDiv = document.createElement('div');
+    phoneDiv.classList.add('phone', 'rounded-lg', 'shadow-lg');
+    phoneDiv.id = 'phone';
+    phoneDiv.innerHTML = `
         <div class="card bg-white  shadow-sm mt-16">
     <figure>
     <img class="w-1/2 h-1/2 mt-5"
@@ -29,15 +29,27 @@ function displayPhones(searchText) {
   </div>
 </div>
     `;
-        phoneContainer.appendChild(phoneDiv);
+    phoneContainer.appendChild(phoneDiv);
 
-    }
-    
+  }
+  loadspiner(false);
+
 
 };
 const searchPhone = async () => {
-    const searchField = document.getElementById('search-phn');
-    const searchText = searchField.value;
-    searchField.value = '';
-    loadPhone(searchText);
+  loadspiner(true);
+  const searchField = document.getElementById('search-phn');
+  const searchText = searchField.value;
+  searchField.value = '';
+  loadPhone(searchText);
 };
+
+const loadspiner = isLoading => {
+  const loaderSection = document.getElementById('loader');
+  if (isLoading) {
+    loaderSection.classList.remove('hidden');
+  }
+  else {
+    loaderSection.classList.add('hidden');
+  }
+}
