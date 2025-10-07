d0084c477d3d48c2e71e59c43a3d0e518d6bdc02
Shakil Haque <shakilhaquepy@gmail.com>
2025-10-07 10:54:24 +0600

    add

--- app/js/app.js
+++ app/js/app.js
@@ -9,7 +9,7 @@ const loadPhone = async (searchText = "13", ishowAll) => {
 
 
 function displayPhones(phones, ishowAll) {
-    console.log(phones);
+    // console.log(phones);
     const phoneContainer = document.getElementById('phone-container');
     phoneContainer.textContent = '';
     const phonelength = phones.length;
@@ -41,7 +41,7 @@ function displayPhones(phones, ishowAll) {
   <div class="card-body">
     <h2 class="flex justify-center m-10 card-title font-black text-black">${phone.phone_name}</h2>
     <div class=" card-actions justify-end">
-      <button onclick="loadPhoneDetails(${phone.slug})" class="btn btn-primary w-full bg-amber-500 text-white 
+      <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-primary w-full bg-amber-500 text-white 
         
       }">showdetails</button>
     </div>
@@ -59,31 +59,51 @@ const loadPhoneDetails = async (slug) => {
     const res = await fetch(url);
     const data = await res.json();
     const phoneDetails = data.data;
-    console.log(phoneDetails);
+    // console.log(phoneDetails);
+    displayPhoneDetails(phoneDetails);
 };
+const displayPhoneDetails = (phoneDetails) => {
+    const modalTitle = document.getElementById('my_modal_1');
+    modalTitle.innerHTML = `
+    <div class="modal-box">
+    <h3 class="text-lg font-bold">${phoneDetails.name}</h3>
+    <img class="w-1/2 h-1/2 mt-5"   src=${phoneDetails.image} />
+    <p class="py-4">Release Date: ${phoneDetails.releaseDate ? phoneDetails.releaseDate : 'No release date found'}</p>
+    <p class="py-4">Storage: ${phoneDetails.mainFeatures ? phoneDetails.mainFeatures.storage : 'No storage information found'}</p>  
+    <p class="py-4">Bluetooth: ${phoneDetails.others ? phoneDetails.others.Bluetooth : 'No Bluetooth information found'}</p>
+    <div class="modal-action">
+        <form method="dialog">  
+            <!-- if there is a button in form, it will close the modal -->
+            <button class="btn">Close</button>
+        </form>
+    </div>  
+    </div>
+    `;
+    modalTitle.showModal();
+}
 
-const searchPhone = async (ishowAll) => {
-    loadspiner(true);
-    const searchField = document.getElementById('search-phn');
-    const searchText = searchField.value;
-    // searchField.value = '';
-    loadPhone(searchText, ishowAll);
-};
+    const searchPhone = async (ishowAll) => {
+        loadspiner(true);
+        const searchField = document.getElementById('search-phn');
+        const searchText = searchField.value;
+        // searchField.value = '';
+        loadPhone(searchText, ishowAll);
+    };
 
-const loadspiner = (isLoading) => {
-    const loaderSection = document.getElementById('loader');
-    // loaderSection.classList.remove('hidden');
-    if (isLoading) {
-        loaderSection.classList.remove('hidden');
+    const loadspiner = (isLoading) => {
+        const loaderSection = document.getElementById('loader');
+        // loaderSection.classList.remove('hidden');
+        if (isLoading) {
+            loaderSection.classList.remove('hidden');
 
-    }
-    else {
-        loaderSection.classList.add('hidden');
+        }
+        else {
+            loaderSection.classList.add('hidden');
 
-    }
-};
-const showAllbtn = () => {
-    searchPhone(true);
-};
+        }
+    };
+    const showAllbtn = () => {
+        searchPhone(true);
+    };
 
-loadPhone();
+    loadPhone();
